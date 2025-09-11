9dc02bd607a4fe4c8ec3d4eefc36aee9c5ed07d2
DucVanD <dvanducw@gmail.com>
2025-09-11 20:06:44 +0700

    project chuyen de lan 1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body class="flex items-center justify-center min-h-screen bg-gray-100">
+    <h1 class="text-red-800 text-4xl text-center">HeloWOk</h1>
+</body>
+</html>
