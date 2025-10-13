4a47d94a4daf07c140318f10fbc7eef82ae4a10e
DucVanD <dvanducw@gmail.com>
2025-10-13 23:51:41 +0700

    add4

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/run-seed', function () {
+//     Artisan::call('migrate:fresh --seed');
+//     return response()->json(['status' => true, 'message' => 'Seed completed!']);
+// });
