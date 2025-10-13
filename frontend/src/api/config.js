681af7101bb3ea6ff2fba09087b0ec9e384e575c
DucVanD <dvanducw@gmail.com>
2025-10-14 01:42:03 +0700

    deploy

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -2,5 +2,7 @@
 // export const apiURL = "http://127.0.0.1:8000/api"; // Laravel thường chạy ở cổng 8000
 // export const imageURL = "http://localhost:8000/assets/images";
 
-export const apiURL = "https://chuyen-de-thuc-tap.onrender.com/api";
-export const imageURL = "https://chuyen-de-thuc-tap.onrender.com/assets/images/";
+// export const apiURL = "https://chuyen-de-thuc-tap.onrender.com/api";
+// export const imageURL = "https://chuyen-de-thuc-tap.onrender.com/assets/images/";
+export const apiURL = import.meta.env.VITE_API_URL;
+export const imageURL = import.meta.env.VITE_IMAGE_URL;
