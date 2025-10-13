26051a9bc31ceb5eca90e39eb226259b968ff303
DucVanD <dvanducw@gmail.com>
2025-10-14 01:11:02 +0700

    Add /check-db route to verify DB connection

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,4 +1,6 @@
-// Đường dẫn API (Laravel backend)
-export const apiURL = "https://chuyen-de-thuc-tap.onrender.com/api"; // Laravel thường chạy ở cổng 8000
-export const imageURL = "http://localhost:8000/assets/images";
+// // Đường dẫn API (Laravel backend)
+// export const apiURL = "http://127.0.0.1:8000/api"; // Laravel thường chạy ở cổng 8000
+// export const imageURL = "http://localhost:8000/assets/images";
 
+export const apiURL = "https://chuyen-de-thuc-tap.onrender.com/api";
+export const imageURL = "https://chuyen-de-thuc-tap.onrender.com/assets/images/";
